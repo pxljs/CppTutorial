ad95c518e629c761121b5acba328f76113ba5b32
luoyongbai <1621352782@qq.com>
2024-10-29 11:24:15 +0800

    Basic System

---
+++ Source/TimePower/TimeManager.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "TimeManager.generated.h"
+
+UCLASS()
+class TIMEPOWER_API ATimeManager : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ATimeManager();
+
+	UFUNCTION(BlueprintCallable)
+	float GetTimeFactor();
+
+	// Time reverse control
+	UFUNCTION(BlueprintCallable)
+	void BeginTimeReverse();
+	UFUNCTION(BlueprintCallable)
+	void EndTimeReverse();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// This is the current active factor of time
+	UPROPERTY(BlueprintReadOnly)
+	float CurrentTimeFactor = 1.0f;
+
+	// Magic time values
+	UPROPERTY(BlueprintReadWrite)
+	float NormalTimeFactor = 1.0f;
+	UPROPERTY(BlueprintReadWrite)
+	float ReverseTimeFactor = -3.0f;
+};
